2b58f93be8ebb252555d1934bd90ae23a4d0abe8
weixiao <sl1222@qq.com>
2019-04-13 16:09:43 +0800

    删除travel

--- travel/src/main/java/com/wanjia/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.wanjia.constant;
-
-public class Constant {
-    public static final String UNACTIVE = "N";
-    public static final String ACTIVED = "Y";
-    public static final String CHECKCODE = "checkcode";
-    public static final Integer PAGESIZE = 8;
-}
--- travel/src/main/java/com/wanjia/dao/ICategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.wanjia.dao;
-
-import com.wanjia.domain.Category;
-
-import java.util.List;
-
-public interface ICategoryDao {
-    List<Category> findCategory()throws Exception;
-}
